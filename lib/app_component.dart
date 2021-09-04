616877a41321630ba18be71fbf58eab2cfe7f6d6
Petr P <petr.mvd+git@gmail.com>
2021-09-04 18:32:01 +0100

    Skeleton using Angular Components

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,9 +1,20 @@
 import 'package:angular/angular.dart';
+import 'package:angular_components/angular_components.dart';
 
 @Component(
-  selector: 'my-app',
-  template: '<h1>Hello {{name}}</h1>',
+  selector: 'mat-drawer-demo',
+  directives: [
+    DeferredContentDirective,
+    MaterialButtonComponent,
+    MaterialIconComponent,
+    MaterialTemporaryDrawerComponent,
+    MaterialListComponent,
+    MaterialListItemComponent,
+  ],
+  templateUrl: 'app_layout_example.html',
+  styleUrls: [
+    'package:angular_components/app_layout/layout.scss.css',
+  ],
 )
 class AppComponent {
-  var name = 'Angular';
 }
