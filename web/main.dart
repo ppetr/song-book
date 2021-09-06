54b9e8314255636cbe3e7b45bcabf110e8be4dc1
Petr Pudlak <petr.mvd+git@gmail.com>
2021-09-06 20:31:13 +0200

    Rename the project to "song-book" and set version to 0.0.1

--- web/main.dart
+++ web/main.dart
@@ -1,5 +1,5 @@
 import 'package:angular/angular.dart';
-import 'package:bhajan_book/app_component.template.dart' as ng;
+import 'package:song_book/app_component.template.dart' as ng;
 import 'package:pwa/client.dart' as pwa;
 
 void main() {
